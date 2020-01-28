0d48ed346076c6274b8d16c7d02232bef8a5dd09
Noblepal <zeronillzero@gmail.com>
2020-01-28 22:44:51 +0300

    Close icon larger

--- app/src/main/java/com/paddy/chatheads/ChatHeadActivity.java
+++ app/src/main/java/com/paddy/chatheads/ChatHeadActivity.java
@@ -6,8 +6,10 @@ import android.net.Uri;
 import android.os.Build;
 import android.os.Bundle;
 import android.provider.Settings;
+import android.util.Log;
 import android.view.View;
 import android.view.WindowManager;
+import android.widget.Toast;
 
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.viewpager.widget.ViewPager;
@@ -24,20 +26,15 @@ public class ChatHeadActivity extends AppCompatActivity {
     ViewPager viewPager;
     //    private BubblesManager bubblesManager;
     private NotificationBadge mBadge;
-
     private com.paddy.chatheads.PermissionChecker mPermissionChecker;
+    private static final String TAG = "ChatHeadActivity";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
 
-        if (getResources().getConfiguration().orientation == Configuration.ORIENTATION_LANDSCAPE) {
-            getWindow().addFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN);
-        } else {
-            getWindow().clearFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN);
-        }
         setContentView(R.layout.activity_chat_head);
-
+        checkPermission();
 
         imageView = findViewById(R.id.avatar2);
         imageView.setOnClickListener(new View.OnClickListener() {
@@ -45,62 +42,40 @@ public class ChatHeadActivity extends AppCompatActivity {
             public void onClick(View v) {
                 initBubble();
                 finish();
-//                Intent intent = new Intent(ChatHeadActivity.this, MainActivity.class);
-//                intent.putExtra("show", "show");
-//                startActivity(intent);
-//                finish();
             }
         });
-        mPermissionChecker = new PermissionChecker(ChatHeadActivity.this);
-        if (!mPermissionChecker.isRequiredPermissionGranted()) {
-            Intent intent = mPermissionChecker.createRequiredPermissionIntent();
-            startActivityForResult(intent, REQUIRED_PERMISSION_REQUEST_CODE);
-        } else {
-        }
-        //Check permissio
-        if (Build.VERSION.SDK_INT >= 23) {
-            if (!Settings.canDrawOverlays(ChatHeadActivity.this)) {
-                Intent intent = new Intent(Settings.ACTION_MANAGE_OVERLAY_PERMISSION,
-                        Uri.parse("package:" + getPackageName()));
-                startActivityForResult(intent, REQUIRED_PERMISSION_REQUEST_CODE);
-            }
-        } else {
-        }
-
 
         // Find the view pager that will allow the user to swipe between fragments
         viewPager = findViewById(R.id.my2viewpager);
-
         // Create an adapter that knows which fragment should be shown on each page
         MyFragmentPagerAdapter adapter = new MyFragmentPagerAdapter(getApplicationContext(), getSupportFragmentManager());
-
         // Set the adapter onto the view pager
         viewPager.setAdapter(adapter);
-
         // Give the TabLayout the ViewPager
         TabLayout tabLayout = findViewById(R.id.my2tablayout);
         tabLayout.setupWithViewPager(viewPager);
 
     }
 
+    private void checkPermission() {
+        if (Build.VERSION.SDK_INT >= 23) {
+            if (!Settings.canDrawOverlays(ChatHeadActivity.this)) {
+                final Intent intent = new Intent(Settings.ACTION_MANAGE_OVERLAY_PERMISSION,
+                        Uri.parse("package:" + getPackageName()));
+                Toast.makeText(this, "Please enable permission", Toast.LENGTH_SHORT).show();
+                startActivityForResult(intent, REQUIRED_PERMISSION_REQUEST_CODE);
+            }
+        } else {
+            Log.d(TAG, "checkPermission: granted");
+        }
+    }
+
     public void initBubble() {
         startService(new Intent(ChatHeadActivity.this, HeadService.class));
     }
 
-    private void addNewBubble() {
-//        final BubbleLayout bubbleView = (BubbleLayout) LayoutInflater.from(ChatHeadActivity.this)
-//                .inflate(R.layout.bubble_layout, null);
-//        mBadge = (NotificationBadge) bubbleView.findViewById(R.id.count);
-//        mBadge.setNumber(88);
-//
-//        bubbleView.setShouldStickToWall(true);
-//        bubblesManager.addBubble(bubbleView, 60, 20);
-
-    }
-
     @Override
     public void onBackPressed() {
-//        addNewBubble();
         super.onBackPressed();
     }
 
